c96b29023c4c772ad7b61c3540aa69115cdaa0e3
Yuvaraj.Gunisetti <yuvaraj.gunisetti@gbgplc.com>
2019-08-01 10:00:34 +0100

    added shortErrorMessage solr field and added progress bar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SolrclientService } from '../solrclient.service';
-import { isFulfilled } from 'q';
 import {TestSummary} from '../models/TestSummary';
 @Component({
   selector: 'app-dashboard',
@@ -11,55 +10,58 @@ export class DashboardComponent implements OnInit {
 
   services = [];
   progress = 0;
+  isFulfilled = false;
 
   constructor(private serviceClient: SolrclientService) { 
     var testSumamryPromise =  serviceClient.getTestExecutionSumamry();
-    var isFulfilled = false;
+    this.isFulfilled = false;
     testSumamryPromise.subscribe(
       (response: TestSummary) => {
-      var pivot = response['facet_counts'].facet_pivot;
-      var servicesStats = pivot['servicename,status'];
-      this.services = [];
-      servicesStats.forEach(serviceStats=> {
-        var name = serviceStats.value;
-        var totalTests = serviceStats.count;
-        var passedTests = 0;
-        var failedTests = 0
-        var skippedTests = 0;
-        for(var item of serviceStats.pivot) {
-          if(item.value === "passed") {
-            passedTests = item.count;
-          } else if(item.value === "failed") {
-            failedTests = item.count;
-          } else if(item.value === "skipped") {
-            skippedTests = item.count;
-          }
-        }
-        var testSummary = {
-          name: name,
-          totalTests: totalTests,
-          totalPass: passedTests,
-          totalFail: failedTests,
-          totalSkipped: skippedTests,
-          passPercentage: ((passedTests/totalTests)*100).toFixed(1),
-          failPercentage: ((failedTests/totalTests)*100).toFixed(1)
-        };
-        this.services.push(testSummary);
-    })
-  },
-   error => console.log(error)
+            var pivot = response['facet_counts'].facet_pivot;
+            var servicesStats = pivot['servicename,status'];
+            this.services = [];
+            servicesStats.forEach(serviceStats=> {
+              var name = serviceStats.value;
+              var totalTests = serviceStats.count;
+              var passedTests = 0;
+              var failedTests = 0
+              var skippedTests = 0;
+              for(var item of serviceStats.pivot) {
+                if(item.value === "passed") {
+                  passedTests = item.count;
+                } else if(item.value === "failed") {
+                  failedTests = item.count;
+                } else if(item.value === "skipped") {
+                  skippedTests = item.count;
+                }
+              }
+              var testSummary = {
+                name: name,
+                totalTests: totalTests,
+                totalPass: passedTests,
+                totalFail: failedTests,
+                totalSkipped: skippedTests,
+                passPercentage: ((passedTests/totalTests)*100).toFixed(1),
+                failPercentage: ((failedTests/totalTests)*100).toFixed(1)
+              };
+              this.services.push(testSummary);
+          });
+          this.isFulfilled = true;
+    },
+   error => console.log(error),
+   () => { this.isFulfilled = true;}
   );
-    this.updateProgressBar();
+  this.updateProgressBar();
   }
   private delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   private async updateProgressBar() {
-    if(isFulfilled) {
+    if(this.isFulfilled) {
       this.progress = 100;
     } else {
-      while (isFulfilled) {
-        console.log(isFulfilled);
+      while (!this.isFulfilled) {
+        console.log(this.isFulfilled);
         this.progress = this.progress+10;
         await this.delay(100);
       }
